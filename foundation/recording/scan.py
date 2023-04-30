427d1f21c70cf68c494673223e78d212069f7f87
Eric Y. Wang <ericwang919@gmail.com>
2023-04-30 15:35:00 -0500

    foundation

--- foundation/recording/scan.py
+++ foundation/recording/scan.py
@@ -1,14 +1,25 @@
 import numpy as np
 import datajoint as dj
+from djutils import merge, skip_missing
+from foundation.scan import trial as scan_trial, unit as scan_unit
 from foundation.recording import trial, trace
-from foundation.schemas.pipeline import pipe_exp
 from foundation.schemas import recording as schema
 
 
 @schema
 class ScanTrialSet(dj.Computed):
     definition = """
-    -> pipe_exp.Scan
+    -> scan_trial.FilteredTrials
     ---
     -> trial.TrialSet
     """
+
+    @skip_missing
+    def make(self, key):
+        # filtered scan trials
+        trials = scan_trial.TrialSet & (scan_trial.FilteredTrials & key)
+        trials = merge(trials.members, trial.TrialLink.ScanTrial)
+
+        # trial set
+        trials = trial.TrialSet.fill(trials, prompt=False)
+        self.insert1(dict(key, **trials))
