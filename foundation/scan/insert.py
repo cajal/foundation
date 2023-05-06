1e520f86c183d2ceb1152e6db886d0906fa192c0
Eric Y. Wang <ericwang919@gmail.com>
2023-05-06 10:32:44 -0500

    foundation

--- foundation/scan/insert.py
+++ foundation/scan/insert.py
@@ -1,1 +1,34 @@
 from djutils import keys
+from foundation.virtual.bridge import pipe_exp, pipe_eye, pipe_shared
+from foundation.scan.experiment import Scan as ScanExperiment
+from foundation.scan.pupil import PupilTrace, PupilNans
+from foundation.scan.trial import TrialFilterSet, FilteredTrials
+from foundation.scan.unit import UnitFilterSet, FilteredUnits
+
+
+@keys
+class Scan:
+    """Scan experiment"""
+
+    @property
+    def key_list(self):
+        return [
+            pipe_exp.Scan,
+            pipe_eye.FittedPupil,
+            TrialFilterSet,
+            UnitFilterSet,
+        ]
+
+    def fill(self):
+        # scan timing
+        ScanExperiment.populate(self.key, reserve_jobs=True, display_progress=True)
+
+        # scan pupil
+        PupilTrace.populate(self.key, reserve_jobs=True, display_progress=True)
+        PupilNans.populate(self.key, reserve_jobs=True, display_progress=True)
+
+        # scan trial set
+        FilteredTrials.populate(self.key, reserve_jobs=True, display_progress=True)
+
+        # scan unit set
+        FilteredUnits.populate(self.key, reserve_jobs=True, display_progress=True)
