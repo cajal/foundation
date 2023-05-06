667a27415b3f295fe366f38afaa42c0aba7f41d3
Eric Y. Wang <ericwang919@gmail.com>
2023-05-06 18:14:40 -0500

    foundation

--- deploy/scripts/recording.py
+++ deploy/scripts/recording.py
@@ -1,4 +1,4 @@
 from foundation.recording.insert import *
 
-key = ScanUnitCache & "animal_id=27203" & utility.ResampleLink.Hamming
+key = ScanUnitCache & "animal_id=27203 and session=4 and scan_idx=7" & utility.ResampleLink.Hamming
 key.fill(12)
