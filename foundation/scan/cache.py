0ecff61499307390b898d098485fc2a564401b0d
Eric Y. Wang <ericwang919@gmail.com>
2023-05-07 14:00:52 -0500

    foundation

--- foundation/scan/cache.py
+++ foundation/scan/cache.py
@@ -1,5 +1,5 @@
 import numpy as np
-from djutils import Filepath
+from djutils import Filepath, rowmethod
 from foundation.virtual.bridge import pipe_shared
 from foundation.scan.unit import UnitSet
 from foundation.schemas import scan as schema
@@ -26,3 +26,8 @@ class UnitsActivity(Filepath):
 
         # insert key
         self.insert1(dict(key, activity=filepath))
+
+    @rowmethod
+    def load(self, checksum=True):
+        filepath = self.filepath("activity", checksum=checksum)
+        return np.load(filepath, mmap_mode="r")
