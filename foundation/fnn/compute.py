799783339a4c5ac2c5c6a1491a828227c529b328
Eric Y. Wang <ericwang919@gmail.com>
2023-05-20 11:24:27 -0500

    foundation

--- foundation/fnn/compute.py
+++ foundation/fnn/compute.py
@@ -77,7 +77,7 @@ class ResampledVisualRecording:
         trials = merge(self.trials, ResampledTraces & key & "finite")
         trial_id, traces = trials.fetch("trial_id", "traces", order_by="trial_id")
 
-        data = [NpyFile(t, transform=transform, dtype=np.float32) for t in tqdm(traces, decs="Traces")]
+        data = [NpyFile(t, transform=transform, dtype=np.float32) for t in tqdm(traces, desc="Traces")]
         return pd.Series(data=data, index=pd.Index(trial_id, name="trial_id"))
 
     @rowproperty
