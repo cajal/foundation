5b83c98cc585c0397280d521842b79e1c160f2f2
Eric Y. Wang <ericwang919@gmail.com>
2023-05-20 14:30:34 -0500

    foundation

--- deploy/scripts/model.py
+++ deploy/scripts/model.py
@@ -1,4 +1,7 @@
+import time
+import random
 from foundation.fnn.model import NetworkModel
 
 if __name__ == "__main__":
+    time.sleep(random.randint(0, 10))
     NetworkModel.populate(reserve_jobs=True)
