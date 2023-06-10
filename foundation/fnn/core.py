aad86f34b3422f733ecb172786d8d8bab96fda09
Eric Y. Wang <ericwang919@gmail.com>
2023-06-10 00:41:09 -0500

    foundation

--- foundation/fnn/core.py
+++ foundation/fnn/core.py
@@ -25,22 +25,29 @@ class _Feedforward:
 
 
 @schema.lookup
-class Res3d(_Feedforward):
+class SpatialTemporalResidual(_Feedforward):
     definition = """
     channels        : varchar(128)  # layer channels (csv)
-    kernel_sizes    : varchar(128)  # layer kernel sizes (csv)
-    strides         : varchar(128)  # layer strides (csv)
+    spatial_sizes   : varchar(128)  # layer spatial sizes (csv)
+    spatial_strides : varchar(128)  # layer spatial strides (csv)
+    temporal_sizes  : varchar(128)  # layer temporal sizes (csv)
     nonlinear       : varchar(128)  # nonlinearity
     """
 
     @rowproperty
     def nn(self):
-        from fnn.model.feedforwards import Res3d
+        from fnn.model.feedforwards import SpatialTemporalResidual
 
-        c, k, s, n = self.fetch1("channels", "kernel_sizes", "strides", "nonlinear")
-        c, k, s = (list(map(int, _.split(","))) for _ in [c, k, s])
-
-        return Res3d(channels=c, kernel_sizes=k, strides=s, nonlinear=n)
+        channels, spatial_sizes, spatial_strides, temporal_sizes, nonlinear = self.fetch1(
+            "channels", "spatial_sizes", "spatial_strides", "temporal_sizes", "nonlinear"
+        )
+        return SpatialTemporalResidual(
+            channels=channels.split(","),
+            spatial_sizes=spatial_sizes.split(","),
+            spatial_strides=spatial_strides.split(","),
+            temporal_sizes=temporal_sizes.split(","),
+            nonlinear=nonlinear,
+        )
 
 
 # -- Feedforward --
@@ -48,7 +55,7 @@ class Res3d(_Feedforward):
 
 @schema.link
 class Feedforward:
-    links = [Res3d]
+    links = [SpatialTemporalResidual]
     name = "feedforward"
 
 
@@ -132,11 +139,11 @@ class FeedforwardRecurrent(_Core):
     def nn(self):
         from fnn.model.cores import FeedforwardRecurrent
 
-        f = (Feedforward & self).link.nn
-        r = (Recurrent() & self).link.nn
-        c = self.fetch1("channels")
-
-        return FeedforwardRecurrent(feedforward=f, recurrent=r, channels=c)
+        return FeedforwardRecurrent(
+            feedforward=(Feedforward & self).link.nn,
+            recurrent=(Recurrent() & self).link.nn,
+            channels=self.fetch1("channels"),
+        )
 
 
 # -- Core --
