98ec824ec7656ed10fa1284b27c5d996c4621f22
Eric Y. Wang <ericwang919@gmail.com>
2023-06-25 13:14:56 -0500

    foundation

--- foundation/function/scan.py
+++ foundation/function/scan.py
@@ -1,4 +1,4 @@
-from djutils import merge, U
+from djutils import U
 from foundation.virtual.bridge import pipe_fuse
 from foundation.virtual import utility, recording, fnn
 from foundation.function.response import RecordingResponse, FnnRecordingResponse, Response, ResponseSet
@@ -8,13 +8,13 @@ from foundation.schemas import function as schema
 @schema.computed
 class VisualScanFnnRecordingResponse:
     definition = """
-    -> pipe_fuse.ScanSet.Unit
-    -> fnn.NetworkModel
-    -> recording.TrialFilterSet
-    -> utility.Bool.proj(trial_perspectives="bool")
-    -> utility.Bool.proj(trial_modulations="bool")
+    -> pipe_fuse.ScanSet.Unit                       # scan unit
+    -> fnn.NetworkModel                             # network model
+    -> recording.TrialFilterSet                     # trial filter
+    -> utility.Bool.proj(trial_perspectives="bool") # trial perspectives
+    -> utility.Bool.proj(trial_modulations="bool")  # trial modulations
     ---
-    -> ResponseSet                  # model/recording response pair
+    -> ResponseSet                                  # fnn/recording response pair
     """
 
     @property
@@ -31,44 +31,44 @@ class VisualScanFnnRecordingResponse:
         key *= recording.TrialFilterSet.proj()
         key *= utility.Bool.proj(trial_perspectives="bool")
         key *= utility.Bool.proj(trial_modulations="bool")
-        
+
         return U(*self.primary_key) & key
 
     def make(self, key):
-        print(key)
+        # unit key
+        unit = (
+            fnn.VisualScanNetwork
+            * fnn.VisualScan
+            * fnn.Spec.VisualSpec
+            * fnn.VisualSpec
+            * recording.ScanTrials
+            * recording.ScanUnits
+            * recording.TraceSet.Member
+            * recording.Trace.ScanUnit
+            * recording.ScanUnit
+        )
+        unit = unit.proj(
+            "trialset_id",
+            "traceset_index",
+            data_filterset_id="trial_filterset_id",
+            resample_id="unit_resample_id",
+            offset_id="unit_offset_id",
+            standardize_id="unit_standardize_id",
+        )
+        unit = (unit & key).fetch1()
+        unit.update(response_index=unit["traceset_index"], **key)
 
-    # def make(self, key):
-    #     # unit key
-    #     unit_key = merge(
-    #         fnn.VisualScanUnit & key,
-    #         recording.ScanUnit,
-    #         recording.Trace.ScanUnit,
-    #         recording.ScanTrials,
-    #         recording.TrialSet,
-    #         fnn.VisualScanNetwork,
-    #         fnn.VisualScan,
-    #         fnn.Spec.VisualSpec,
-    #         fnn.VisualSpec,
-    #     )
-    #     unit_key = unit_key.proj(
-    #         resample_id="unit_resample_id",
-    #         offset_id="unit_offset_id",
-    #         standardize_id="unit_standardize_id",
-    #     )
-    #     unit_key = unit_key.fetch1()
-    #     unit_key.update(key)
+        # insert responses
+        RecordingResponse.insert1(unit, ignore_extra_fields=True, skip_duplicates=True)
+        FnnRecordingResponse.insert1(unit, ignore_extra_fields=True, skip_duplicates=True)
 
-    #     # insert responses
-    #     RecordingResponse.insert1(unit_key, ignore_extra_fields=True, skip_duplicates=True)
-    #     FnnRecordingResponse.insert1(unit_key, ignore_extra_fields=True, skip_duplicates=True)
+        # fill responses
+        Response.fill()
 
-    #     # fill responses
-    #     Response.fill()
+        # response pair
+        pair = [Response.RecordingResponse, Response.FnnRecordingResponse]
+        pair = [(table & unit).fetch1("KEY") for table in pair]
+        pair = ResponseSet.fill(pair, prompt=False, silent=True)
 
-    #     # response pair
-    #     pair = [Response.RecordingResponse, Response.FnnRecordingResponse]
-    #     pair = [(table & unit_key).fetch1("KEY") for table in pair]
-    #     pair = ResponseSet.fill(pair, prompt=False, silent=True)
-
-    #     # insert key
-    #     self.insert1(dict(key, **pair), ignore_extra_fields=True)
+        # insert key
+        self.insert1(dict(key, **pair), ignore_extra_fields=True)
