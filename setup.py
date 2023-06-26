c8cb32b073b2cd4334c478e0f82c3b8b470ce839
Eric Y. Wang <ericwang919@gmail.com>
2023-06-26 00:28:47 -0500

    foundation

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup, find_packages
 setup(
     name="foundation",
     version="0",
-    description="Foundation Models of the Visual Cortex",
+    description="Foundation Pipeline",
     author="Eric Y. Wang",
     author_email="eric.wang2@bcm.edu",
-    packages=find_packages(exclude=[]),
-    install_requires=[],
+    packages=find_packages(),
+    install_requires=["datajoint"],
 )
