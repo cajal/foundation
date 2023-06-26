47874f8b4a3b08adb2deee045b55ce6048f32ae0
Eric Y. Wang <ericwang919@gmail.com>
2023-06-26 08:39:49 -0500

    foundation

--- setup.py
+++ setup.py
@@ -9,5 +9,7 @@ setup(
     author="Eric Y. Wang",
     author_email="eric.wang2@bcm.edu",
     packages=find_packages(),
-    install_requires=["datajoint"],
+    install_requires=[
+        "datajoint>=0.12.9,<0.13.0",
+    ],
 )
