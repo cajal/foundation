d482f671a718152184eb143b50c76ff3153ab679
Eric Y. Wang <ericwang919@gmail.com>
2023-07-13 18:50:35 -0500

    VisualScanPerformance

--- foundation/fnn/compute_scan.py
+++ foundation/fnn/compute_scan.py
@@ -1,26 +1,48 @@
 from djutils import keys, merge, rowproperty
-from foundation.virtual import recording, fnn
+from foundation.virtual import utility, stimulus, recording, fnn
 
 
 @keys
-class VisualScanNetwork:
-    """Visual Scan Network"""
+class VisualScanPerformance:
+    """Visual Scan CC Norm"""
 
     @property
     def keys(self):
         return [
-            fnn.VisualScanNetwork,
+            fnn.NetworkModel & (fnn.Network.VisualNetwork & fnn.Data.VisualScan),
+            utility.Bool.proj(trial_perspective="bool"),
+            utility.Bool.proj(trial_modulation="bool"),
+            recording.TrialFilterSet,
+            stimulus.VideoSet,
+            utility.Burnin,
         ]
 
-    @rowproperty
-    def unit_traces(self):
-        from foundation.recording.trace import Trace, TraceSet
+    def cc_norm(self):
+        # key
+        key = self.key.fetch(as_dict=True)
 
-        # unit traceset key
-        key = merge(self.key, fnn.VisualScanNetwork, recording.ScanUnits)
+        # data
+        data = fnn.Network.VisualNetwork.proj("data_id") * fnn.Data.VisualScan & key
 
-        # traceset members
-        units = (TraceSet & key).members
+        # data specfication
+        spec = (fnn.Spec.VisualSpec & data).proj(
+            "rate_id",
+            resample_id="unit_resample_id",
+            offset_id="unit_offset_id",
+            standardize_id="unit_standardize_id",
+        )
 
-        # unit traces
-        return merge(units, Trace.ScanUnit)
+        # traces
+        traces = data * recording.ScanUnits * recording.TraceSet.Member * recording.Trace.ScanUnit * recording.ScanUnit
+        traces = traces.proj("traceset_index")
+
+        # CC max
+        cc_max = (recording.VisualMeasure & utility.Measure.CCMax & key & spec) * traces
+        cc_max = cc_max.proj("traceset_index", cc_max="measure")
+
+        # CC abs
+        cc_abs = fnn.VisualUnitCorrelation & utility.Correlation.CCSignal & key
+        cc_abs = cc_abs.proj(cc_abs="correlation", traceset_index="unit_index")
+
+        # CC norm
+        return (cc_abs * cc_max).proj("cc_abs", "cc_max", cc_norm="cc_abs / cc_max")
