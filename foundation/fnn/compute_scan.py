abc6bc7f73ed9f665e0b522a35c557b9cfa037bd
Eric Y. Wang <ericwang919@gmail.com>
2023-07-14 12:08:50 -0500

    cc_norm

--- foundation/fnn/compute_scan.py
+++ foundation/fnn/compute_scan.py
@@ -1,5 +1,50 @@
 from djutils import keys, merge, rowproperty
-from foundation.virtual import utility, stimulus, recording, fnn
+from foundation.virtual import utility, stimulus, scan, recording, fnn
+
+
+@keys
+class VisualScanModel:
+    """Visual Scan CC Norm"""
+
+    @property
+    def keys(self):
+        return [
+            (scan.Scan * fnn.NetworkModel) & (fnn.Network.VisualNetwork * fnn.Data.VisualScan),
+        ]
+
+    @property
+    def data(self):
+        return self.key * fnn.Network.VisualNetwork.proj("data_id")
+
+    @property
+    def rate(self):
+        key = (fnn.Data.VisualScan * fnn.Spec.VisualSpec).proj("rate_id")
+        return self.data * key
+
+    @property
+    def unit_resample(self):
+        key = (fnn.Data.VisualScan * fnn.Spec.VisualSpec).proj(resample_id="unit_resample_id")
+        return self.data * key
+
+    @property
+    def unit_offset(self):
+        key = (fnn.Data.VisualScan * fnn.Spec.VisualSpec).proj(offset_id="unit_offset_id")
+        return self.data * key
+
+    @property
+    def unit_standardize(self):
+        key = (fnn.Data.VisualScan * fnn.Spec.VisualSpec).proj(standardize_id="unit_standardize_id")
+        return self.data * key
+
+    @property
+    def unit_traces(self):
+        key = (fnn.Data.VisualScan * recording.ScanUnits * recording.TraceSet.Member).proj("traceset_index")
+        return self.data * key
+
+    @property
+    def standardize_trials(self):
+        key = (fnn.Data.VisualScan * recording.ScanTrials).proj("trialset_id")
+        return self.data * key
 
 
 @keys
@@ -9,7 +54,7 @@ class VisualScanPerformance:
     @property
     def keys(self):
         return [
-            fnn.NetworkModel & (fnn.Network.VisualNetwork & fnn.Data.VisualScan),
+            VisualScanModel.key_source,
             utility.Bool.proj(trial_perspective="bool"),
             utility.Bool.proj(trial_modulation="bool"),
             recording.TrialFilterSet,
@@ -22,28 +67,19 @@ class VisualScanPerformance:
         # key
         key = self.key.fetch(as_dict=True)
 
-        # data
-        data = fnn.Network.VisualNetwork.proj("data_id") * fnn.Data.VisualScan & key
-
-        # data specfication
-        spec = (fnn.Spec.VisualSpec & data).proj(
-            "rate_id",
-            resample_id="unit_resample_id",
-            offset_id="unit_offset_id",
-            standardize_id="unit_standardize_id",
-        )
-
-        # traces
-        traces = data * recording.ScanUnits * recording.TraceSet.Member * recording.Trace.ScanUnit * recording.ScanUnit
-        traces = traces.proj("traceset_index")
+        # unit keys
+        rate = (VisualScanModel & key).rate
+        resample = (VisualScanModel & key).unit_resample
+        offset = (VisualScanModel & key).unit_offset
+        traces = (VisualScanModel & key).unit_traces
 
         # CC max
-        cc_max = (recording.VisualMeasure & utility.Measure.CCMax & key & spec) * traces
-        cc_max = cc_max.proj("traceset_index", cc_max="measure")
+        cc_max = recording.VisualMeasure & utility.Measure.CCMax.fetch1("KEY")
+        cc_max = (cc_max * self.key * rate * resample * offset * traces).proj("traceset_index", cc_max="measure")
 
         # CC abs
-        cc_abs = fnn.VisualUnitCorrelation & utility.Correlation.CCSignal & key
-        cc_abs = cc_abs.proj(cc_abs="correlation", traceset_index="unit_index")
+        cc_abs = fnn.VisualUnitCorrelation & utility.Correlation.CCSignal.fetch1("KEY")
+        cc_abs = (cc_abs * self.key).proj(traceset_index="unit_index", cc_abs="correlation")
 
         # CC norm
         return (cc_abs * cc_max).proj("cc_abs", "cc_max", cc_norm="cc_abs / cc_max")
