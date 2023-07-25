5a257a4c455cdf30279163ece7cb30f3f75910bb
Eric Y. Wang <ericwang919@gmail.com>
2023-07-25 09:38:36 -0500

    virtual

--- foundation/virtual/__init__.py
+++ foundation/virtual/__init__.py
@@ -1,7 +1,7 @@
 from datajoint import create_virtual_module
 
-utility = create_virtual_module("utility", "foundation_utility", create_schema=True)
-stimulus = create_virtual_module("stimulus", "foundation_stimulus", create_schema=True)
-scan = create_virtual_module("scan", "foundation_scan", create_schema=True)
-recording = create_virtual_module("recording", "foundation_recording", create_schema=True)
-fnn = create_virtual_module("fnn", "foundation_fnn", create_schema=True)
+utility = create_virtual_module("utility", "foundation_utility")
+stimulus = create_virtual_module("stimulus", "foundation_stimulus")
+scan = create_virtual_module("scan", "foundation_scan")
+recording = create_virtual_module("recording", "foundation_recording")
+fnn = create_virtual_module("fnn", "foundation_fnn")
