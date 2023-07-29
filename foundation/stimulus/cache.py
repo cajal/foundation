fe4feec709c38b325f6f9c811b29981b9ae7a4bf
Eric Y. Wang <ericwang919@gmail.com>
2023-07-29 13:22:15 -0500

    external blob

--- foundation/stimulus/cache.py
+++ foundation/stimulus/cache.py
@@ -5,51 +5,20 @@ from foundation.schemas import stimulus as schema
 
 
 @schema.computed
-class ResizedVideoTemp:
-    definition = """
-    -> Video
-    -> utility.Resize
-    -> utility.Resolution
-    ---
-    video       : blob@external    # npy file, [frames, height, width, channels]
-    """
-
-    @property
-    def key_source(self):
-        return ResizedVideo.proj()
-
-    def make(self, key):
-        v = (ResizedVideo & key).fetch1("video")
-        self.insert1(dict(key, video=np.load(v)))
-
-
-@schema.computed
 class ResizedVideo:
     definition = """
     -> Video
     -> utility.Resize
     -> utility.Resolution
     ---
-    video           : blob@external    # npy file, [frames, height, width, channels]
+    video           : blob@external    # [frames, height, width, channels]
     """
 
-    @property
-    def key_source(self):  # TEMP
-        return ResizedVideoTemp.proj()
+    def make(self, key):
+        from foundation.stimulus.compute_video import ResizedVideo
 
-    def make(self, key):  # TEMP
-        v = (ResizedVideoTemp & key).fetch1("video")
-        self.insert1(dict(key, video=v))
+        # resize video
+        video = (ResizedVideo & key).video
 
-    # def make(self, key):
-    #     from foundation.stimulus.compute_video import ResizedVideo
-
-    #     # resize video
-    #     video = (ResizedVideo & key).video
-
-    #     # save video
-    #     filepath = self.createpath(key, "video", "npy")
-    #     np.save(filepath, video.array)
-
-    #     # insert key
-    #     self.insert1(dict(key, video=filepath))
+        # insert key
+        self.insert1(dict(key, video=video.array))
