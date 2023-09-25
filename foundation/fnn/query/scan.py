c17bf0d8c64200c5f54bab6b11fa5d3fb9277aa0
Eric Y. Wang <ericwang919@gmail.com>
2023-09-25 17:50:05 -0500

    scan units

--- foundation/fnn/query/scan.py
+++ foundation/fnn/query/scan.py
@@ -3,35 +3,51 @@ from foundation.virtual import utility, stimulus, scan, recording, fnn
 
 
 @keys
-class VisualScanCorrelation:
-    """Visual Scan Correlation"""
+class VisualScanRecording:
+    """Visual Scan Recording"""
 
     @property
     def keys(self):
         return [
-            (scan.Scan * fnn.Model) & fnn.Data.VisualScan,
-            recording.TrialFilterSet,
-            stimulus.VideoSet,
-            utility.Burnin,
-            utility.Bool.proj(perspective="bool"),
-            utility.Bool.proj(modulation="bool"),
+            (scan.Scan * fnn.Data) & fnn.Data.VisualScan,
         ]
 
-    def cc_norm(self):
-        spec = fnn.Data.VisualScan.proj(
-            "spec_id", "trace_filterset_id", "pipe_version", "segmentation_method", "spike_method"
-        )
-        units = merge(
-            self.key,
-            spec,
-            fnn.Spec.VisualSpec.proj("rate_id", offset_id="offset_id_unit", resample_id="resample_id_unit"),
-            (fnn.VisualRecordingCorrelation & utility.Correlation.CCSignal).proj(..., trace_order="unit"),
-            recording.ScanUnitOrder,
-            recording.Trace.ScanUnit,
-            recording.VisualMeasure & utility.Measure.CCMax,
-        )
-        return units.proj(
-            cc_abs="correlation",
-            cc_max="measure",
-            cc_norm="correlation/measure",
-        )
+    @property
+    def units(self):
+        units = self.key * fnn.Data.VisualScan * recording.ScanUnitOrder * recording.Trace.ScanUnit * recording.ScanUnit
+        return units.proj("unit_id", "trace_order")
+
+
+# @keys
+# class VisualScanCorrelation:
+#     """Visual Scan Correlation"""
+
+#     @property
+#     def keys(self):
+#         return [
+#             (scan.Scan * fnn.Model) & fnn.Data.VisualScan,
+#             recording.TrialFilterSet,
+#             stimulus.VideoSet,
+#             utility.Burnin,
+#             utility.Bool.proj(perspective="bool"),
+#             utility.Bool.proj(modulation="bool"),
+#         ]
+
+#     def cc_norm(self):
+#         spec = fnn.Data.VisualScan.proj(
+#             "spec_id", "trace_filterset_id", "pipe_version", "segmentation_method", "spike_method"
+#         )
+#         units = merge(
+#             self.key,
+#             spec,
+#             fnn.Spec.VisualSpec.proj("rate_id", offset_id="offset_id_unit", resample_id="resample_id_unit"),
+#             (fnn.VisualRecordingCorrelation & utility.Correlation.CCSignal).proj(..., trace_order="unit"),
+#             recording.ScanUnitOrder,
+#             recording.Trace.ScanUnit,
+#             recording.VisualMeasure & utility.Measure.CCMax,
+#         )
+#         return units.proj(
+#             cc_abs="correlation",
+#             cc_max="measure",
+#             cc_norm="correlation/measure",
+#         )
