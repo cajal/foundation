a56b45590f3efb1aa2201cb6100e663f707d1c80
Eric Y. Wang <ericwang919@gmail.com>
2024-02-16 19:50:13 -0600

    optimize

--- foundation/recording/compute/visual.py
+++ foundation/recording/compute/visual.py
@@ -93,9 +93,7 @@ class VisualMeasure:
 
                 # no trials for video
                 if not trial_ids:
-                    logger.warning(
-                        f"No trials found for video_id `{video['video_id']}`"
-                    )
+                    logger.warning(f"No trials found for video_id `{video['video_id']}`")
                     continue
 
                 # trial responses
@@ -144,102 +142,98 @@ class VisualDirectionTuning:
         list
             number of trials per direction
         """
+        from foundation.recording.trial import (
+            Trial,
+            TrialSet,
+            TrialVideo,
+            TrialBounds,
+            TrialFilterSet,
+        )
         from foundation.recording.trace import Trace
-        from foundation.recording.trial import Trial
         from foundation.stimulus.video import VideoSet, Video
         from foundation.utility.resample import Offset
         from foundation.utility.impulse import Impulse
         from foundation.utility.numeric import Precision
         from foundation.stimulus.compute.video import DirectionType
 
-        # trial set
-        trialset = (recording.TraceTrials & self.item).fetch1()
-
-        # videos
-        video_ids = (VideoSet & self.item).members
-        video_ids = video_ids.fetch("KEY", order_by=video_ids.primary_key)
+        # trace times and values
+        trace = (Trace & self.item).link.compute
+        times, values = trace.times, trace.values
 
         # offset
         offset = (Offset & self.item).link.offset
 
-        # get unit trace time and value
-        trace = (Trace & self.item).link.compute
-        trace_times, trace_values = trace.times, trace.values
+        # impulse
+        impulse = (Impulse & self.item).link.impulse(times, values, offset)
 
-        # initialize Impulse
-        impulse = (Impulse & self.item).link.impulse(trace_times, trace_values, offset)
+        # precision
+        pstr = (Precision & self.item).link.string
 
-        # round for numeric precision
-        round = (Precision & self.item).link.string
+        # videos
+        videos = (VideoSet & self.item).members
 
-        with cache_rowproperty():
-            subtrials = []
+        # trial set
+        trialset = (recording.TraceTrials & self.item).fetch1()
 
-            for video_id in tqdm(video_ids, desc="Videos"):
-                # check if video is a direction type
-                video = (Video & video_id).link.compute
-                assert isinstance(
-                    video, DirectionType
-                ), f"Video `{video_id['video_id']}` is not a direction type"
+        # all trials
+        trials = Trial & (TrialSet & trialset).members
 
-                # get direction onset and offset times within a single trial
-                directions, dir_starts, dir_ends = list(zip(*video.directions()))
-                directions = [round(direction) for direction in directions]
-                dir_starts = np.array(dir_starts)
-                dir_ends = np.array(dir_ends)
+        # filtered trials
+        trials = (TrialFilterSet & self.item).filter(trials)
 
-                # trial ids
-                trial_ids = (VisualTrials & trialset & video_id & self.item).trial_ids
+        # trial info
+        trials = merge(trials, TrialBounds, TrialVideo)
 
-                # loop over trials
-                for trial_id in trial_ids:
-                    # get trial start and end
-                    trial_start, trial_end = (
-                        Trial & dict(trial_id=trial_id)
-                    ).link.compute.bounds
-                    # compute direction start and end times
-                    subtrial_starts = dir_starts + trial_start
-                    subtrial_ends = dir_ends + trial_start
-                    subtrial_starts = np.minimum(subtrial_starts, trial_end)  # clip to trial end
-                    subtrial_ends = np.minimum(subtrial_ends, trial_end)  # clip to trial end
+        # trials restricted by videos
+        trials = trials & videos
+
+        # fetch trials
+        video_ids, starts = trials.fetch("video_id", "start", order_by="start")
+
+        # video dataframe
+        rows = []
+        for video_id in tqdm(np.unique(video_ids), desc="Videos"):
+
+            video = (Video & {"video_id": video_id}).link.compute
+            assert isinstance(video, DirectionType)
+
+            dirs, ons, offs = zip(*video.directions())
+
+            row = {"video_id": video_id, "dirs": list(map(pstr, dirs)), "ons": ons, "offs": offs}
+            rows.append(row)
+
+        vdf = pd.DataFrame(rows).set_index("video_id")
+
+        # response dataframe
+        rows = []
+        for video_id, start in zip(tqdm(video_ids, desc="Trials"), starts):
+
+            vid = vdf.loc[video_id]
+
+            for direction, on, off in zip(vid.dirs, vid.ons, vid.offs):
+
+                row = {"direction": direction, "response": impulse(start + on, start + off)}
+                rows.append(row)
+
+        rdf = pd.DataFrame(rows)
+        rdf = rdf.groupby("direction")
+        rdf = rdf.agg(dict(response=["mean", "count"]))
+        rdf.index = rdf.index.astype(float)
+        rdf = rdf.reset_index()
+        rdf = rdf.sort_values("direction")
+
+        return rdf["direction"].values, rdf["response"]["mean"].values, rdf["response"]["count"].values
 
 
-                    for direction, start, end in zip(
-                        directions, subtrial_starts, subtrial_ends
-                    ):  
-                        subtrials.append(
-                            dict(
-                                direction=direction,
-                                response=impulse(start, end),  # returns nan if no response in time range
-                            )
-                        )
-
-            # no trials at all
-            if not subtrials:
-                logger.warning("No trials found")
-
-            subtrials = pd.DataFrame(subtrials)
-            summary = subtrials.groupby("direction").agg(
-                dict(
-                    response=["mean", "count"],
-                )
-            )
-
-            return (
-                summary.index.to_list(),
-                summary["response"]["mean"].to_numpy(),
-                summary["response"]["count"].to_numpy(),
-            )
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     def test_visual_direction_tuning():
         from foundation.virtual import recording, stimulus, utility
         from foundation.recording.compute.visual import VisualDirectionTuning
-        unit_key = recording.Trace.ScanUnit & 'animal_id=17797 and session=4 and scan_idx=7 and unit_id=1'
+
+        unit_key = recording.Trace.ScanUnit & "animal_id=17797 and session=4 and scan_idx=7 and unit_id=1"
         trial_filter_set = recording.TrialFilterSet & 'trial_filterset_id="d00bbb175d63398818ca652391c18856"'
         video_set = stimulus.VideoSet & 'videoset_id="01eba4d8945512806337d42d015f2780"'
-        offset = utility.Offset.MsOffset & 'ms_offset=150'
+        offset = utility.Offset.MsOffset & "ms_offset=150"
         test = VisualDirectionTuning & unit_key & trial_filter_set & video_set & offset
         return test.tuning
-
