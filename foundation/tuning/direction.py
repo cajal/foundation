39f10c4988551ac4492cbfb10b057e566cd763e2
Zhuokun Ding <zkding@outlook.com>
2024-02-26 15:46:51 +0000

    draft direction tuning pipeline

--- foundation/tuning/direction.py
+++ foundation/tuning/direction.py
@@ -1,5 +1,5 @@
 from djutils import merge, rowproperty, rowmethod
-from foundation.virtual import recording, fnn
+from foundation.virtual import recording, fnn, stimulus, utility
 from foundation.schemas import tuning as schema
 
 
@@ -20,15 +20,20 @@ class DirectionResp:
             compute unit responses to direction stimuli
         """
         raise NotImplementedError()
-    
+
 
 # -- DirectionResp Types --
-    
+
 
 @schema.lookup
 class RecordingDirectionResp(DirectionResp):
     definition = """
-    -> pipe_shared.Recording
+    -> recording.TraceSet
+    -> recording.TrialFilterSet
+    -> stimulus.VideoSet
+    -> utility.Offset
+    -> utility.Impulse
+    -> utility.Precision
     """
 
     @rowproperty
@@ -36,12 +41,17 @@ class RecordingDirectionResp(DirectionResp):
         from foundation.tuning.compute.direction import RecordingDirectionResp
 
         return RecordingDirectionResp & self
-    
+
 
 @schema.lookup
 class FnnDirectionResp(DirectionResp):
     definition = """
     -> fnn.Model
+    -> stimulus.VideoSet
+    -> utility.Burnin
+    -> utility.Offset
+    -> utility.Impulse
+    -> utility.Precision
     """
 
     @rowproperty
@@ -49,10 +59,10 @@ class FnnDirectionResp(DirectionResp):
         from foundation.tuning.compute.direction import FnnDirectionResp
 
         return FnnDirectionResp & self
-    
+
 
 # -- DirectionResp --
-    
+
 
 @schema.link
 class DirectionResp:
@@ -61,27 +71,87 @@ class DirectionResp:
     comment = "unit responses to direction stimuli"
 
 
-# -- OSI --
-    
+# -- gOSI --
+
+
 @schema.computed
-class OSI:
+class GOSI:
     definition = """
     -> DirectionResp
     -> recording.Trace
     ---
-    osi = NULL      : float     # orientation selectivity index
     gosi = NULL     : float     # global orientation selectivity index
     """
 
+    @property
+    def key_source(self):
+        from foundation.tuning.compute.direction import GOSI
 
-# -- DSI --
+        return GOSI.key_source
+
+    def make(self, key):
+        from foundation.tuning.compute.direction import GOSI
+
+        gosi, trace_ids = (GOSI & key).gosi
+        self.insert(
+            [{**key, "trace_id": trace, "gosi": g} for trace, g in zip(trace_ids, gosi)]
+        )
+
+
+# -- gDSI --
+
 
 @schema.computed
-class DSI:
+class GDSI:
     definition = """
     -> DirectionResp
     -> recording.Trace
     ---
-    dsi = NULL      : float     # direction selectivity index
     gdsi = NULL     : float     # global direction selectivity index
     """
+
+    @property
+    def key_source(self):
+        from foundation.tuning.compute.direction import GDSI
+
+        return GDSI.key_source
+
+    def make(self, key):
+        from foundation.tuning.compute.direction import GDSI
+
+        gdsi, trace_ids = (GDSI & key).gdsi
+        self.insert(
+            [{**key, "trace_id": trace, "gdsi": g} for trace, g in zip(trace_ids, gdsi)]
+        )
+
+
+# -- BiVonMisesFit --
+@schema.computed
+class BiVonMises():
+    definition = """
+    -> DirectionResp
+    -> recording.Trace
+    ---
+    success     : bool      # success of least squares optimization
+    mu          : float     # center of the first von Mises distribution
+    phi         : float     # weight of the first von Mises distribution
+    kappa       : float     # dispersion of both von Mises distributions
+    scale       : float     # von Mises amplitude
+    bias        : float     # uniform amplitude
+    mse         : float     # mean squared error
+    osi         : float     # orientation selectivity index
+    dsi         : float     # direction selectivity index
+    """
+
+    @property
+    def key_source(self):
+        from foundation.tuning.compute.direction import BiVonMises
+        return BiVonMises.key_source
+
+    
+    def make(self, key):
+        from foundation.tuning.compute.direction import BiVonMises
+        fit = (BiVonMises & key).fit
+        for k, v in key.items():
+            fit[k] = v
+        self.insert(fit)
