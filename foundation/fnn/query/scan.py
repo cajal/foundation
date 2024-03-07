1ec045cee48f55a0d00e6e8a29fe1189352532ac
Eric Y. Wang <ericwang919@gmail.com>
2024-03-07 11:41:26 -0600

    cc norm

--- foundation/fnn/query/scan.py
+++ foundation/fnn/query/scan.py
@@ -1,4 +1,5 @@
-from djutils import keys, merge
+import pandas as pd
+from djutils import keys, rowproperty
 from foundation.virtual import utility, stimulus, scan, recording, fnn
 
 
@@ -15,40 +16,69 @@ class VisualScanRecording:
     @property
     def units(self):
         key = self.key.fetch("KEY")
-        units = fnn.Data.VisualScan * recording.ScanUnitOrder * recording.Trace.ScanUnit * recording.ScanUnit & key
+        units = (
+            fnn.Data.VisualScan * recording.ScanUnitOrder * recording.Trace.ScanUnit * recording.ScanUnit
+            & key
+        )
         return units.proj("trace_order")
 
 
-# @keys
-# class VisualScanCorrelation:
-#     """Visual Scan Correlation"""
+@keys
+class VisualScanCorrelation:
+    """Visual Scan Correlation"""
 
-#     @property
-#     def keys(self):
-#         return [
-#             (scan.Scan * fnn.Model) & fnn.Data.VisualScan,
-#             recording.TrialFilterSet,
-#             stimulus.VideoSet,
-#             utility.Burnin,
-#             utility.Bool.proj(perspective="bool"),
-#             utility.Bool.proj(modulation="bool"),
-#         ]
+    @property
+    def keys(self):
+        return [
+            (scan.Scan * fnn.Model) & fnn.Data.VisualScan,
+            recording.TrialFilterSet,
+            stimulus.VideoSet,
+            utility.Burnin,
+            utility.Bool.proj(perspective="bool"),
+            utility.Bool.proj(modulation="bool"),
+        ]
 
-#     def cc_norm(self):
-#         spec = fnn.Data.VisualScan.proj(
-#             "spec_id", "trace_filterset_id", "pipe_version", "segmentation_method", "spike_method"
-#         )
-#         units = merge(
-#             self.key,
-#             spec,
-#             fnn.Spec.VisualSpec.proj("rate_id", offset_id="offset_id_unit", resample_id="resample_id_unit"),
-#             (fnn.VisualRecordingCorrelation & utility.Correlation.CCSignal).proj(..., trace_order="unit"),
-#             recording.ScanUnitOrder,
-#             recording.Trace.ScanUnit,
-#             recording.VisualMeasure & utility.Measure.CCMax,
-#         )
-#         return units.proj(
-#             cc_abs="correlation",
-#             cc_max="measure",
-#             cc_norm="correlation/measure",
-#         )
+    @rowproperty
+    def cc_norm(self):
+        from foundation.fnn.data import Data
+
+        data = (Data & self.item).link.compute
+
+        ukey = data.key_unit
+
+        mkey = {
+            "rate_id": ukey["rate_id"],
+            "resample_id": ukey["resample_id"],
+            "offset_id": ukey["offset_id"],
+            "measure_id": utility.Measure.CCMax.fetch1("measure_id"),
+        }
+
+        ckey = {
+            "correlation_id": utility.Correlation.CCSignal.fetch1("correlation_id"),
+        }
+
+        traces = recording.ScanUnitOrder & ukey
+
+        measures = (recording.VisualMeasure * traces & mkey & self.item).proj(
+            cc_max="measure", unit="trace_order"
+        )
+
+        correlations = (fnn.VisualRecordingCorrelation & ckey & self.item).proj(cc_abs="correlation")
+        units = correlations * measures * recording.Trace.ScanUnit
+        assert len(units) == data.units
+
+        cols = [
+            "animal_id",
+            "session",
+            "scan_idx",
+            "pipe_version",
+            "segmentation_method",
+            "spike_method",
+            "unit_id",
+            "cc_abs",
+            "cc_max",
+        ]
+        df = pd.DataFrame(units.fetch(*cols, as_dict=True))[cols]
+        df["cc_norm"] = df.cc_abs / df.cc_max
+
+        return df
